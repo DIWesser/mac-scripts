338458a6d688042b301246d207d0fa2fc50bb88e
Daniel Wesser <daniel@T8B93.WPA.Dal.Ca>
2017-11-05 02:47:43 +0000

    Now it actually does something. It even works.

--- install-scripts.sh
+++ install-scripts.sh
@@ -1,3 +1,11 @@
 #! /bin/bash
 
 # This script should symlink the everything that works
+
+# Scripts are stored in ~/bin
+# To access add the following line to .bashrc/bash_profile
+# export PATH=~/bin:$PATH
+
+# Create directory
+mkdir -p ~/bin
+ln -s ~/Git/mac-scripts/upgrade-all.sh ~/bin/upgrade-all
