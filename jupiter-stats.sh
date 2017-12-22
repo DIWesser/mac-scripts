5e42b2cd396034ee6c66b0bdee4fad573f13f822
Daniel Wesser <daniel@elli.local>
2017-12-22 03:17:01 +0000

    removed line break from storage used

--- jupiter-stats.sh
+++ jupiter-stats.sh
@@ -7,6 +7,6 @@ echo -ne "\033[1mUptime:\033[0m "
 uptime -p | cut -d' ' -f2-20
 
 echo -ne "\033[1mStorage Used:\033[0m "
-df -H | grep md0 | cut -d' ' -f 30-31
+df -H | grep md0 | cut -d' ' -f 30
 echo -ne "/"
 df -H | grep md0 | cut -d' ' -f 27-28
