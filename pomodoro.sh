bdcd9dc4a5665b45c7e40721e78bceccf4c0fce1
Daniel Wesser <daniel@elli.local>
2017-12-24 05:11:53 +0000

    Added manual confirmation for start of break.

--- pomodoro.sh
+++ pomodoro.sh
@@ -32,24 +32,29 @@ workPeriod=$(grep -iw '^\s*work period:' $settings | cut -d: -f2 | xargs)
 #runTimer=true
 #while [ "$runTimer" = true ] ; do
     # Work period
-    osascript -e \
+        osascript -e \
         'display notification "Starting work timer.\nWork for 25 minutes" with title "Pomodoro"'
-    say "Begin."
-    sleep $workPeriod # Sleep 25 minutes
-    echo -ne "\n"
+        say "Begin."
+
+        sleep $workPeriod # Sleep 25 minutes
+        echo
 
     # Break period
-    osascript -e 'display notification "Take a break" with title "Pomodoro"'
-    say "Time to take a break."
-    sleep `expr $shortBreak - 60` # Sleep for short break minus one minute for warning time
-    osascript -e 'display notification "Break ends in 1 minute" with title "Pomodoro"'
-    say "One minute warning."
-    sleep 60
+        osascript -e 'display notification "Take a break" with title "Pomodoro"'
+        say "Time to take a break."
+
+        read -n 1 -s -p "Press any key to start break"
+        echo
+
+        sleep `expr $shortBreak - 60` # Sleep for short break minus one minute for warning time
+        osascript -e 'display notification "Break ends in 1 minute" with title "Pomodoro"'
+        say "One minute warning."
+        sleep 60
 
     # End of break
-    #osascript -e 'tell app "System Events" to display dialog\
-    #    "Time to get back to work. Would you like to restart the timer?"'
-    osascript -e \
-    'display notification "Beak over.\nRestart script to continue timer." with title "Pomodoro"'
-    say "Break period over."
+        #osascript -e 'tell app "System Events" to display dialog\
+        #    "Time to get back to work. Would you like to restart the timer?"'
+        osascript -e \
+        'display notification "Break over.\nRestart script to continue." with title "Pomodoro"'
+        say "Break period over."
 #done
