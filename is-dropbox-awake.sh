1edf97d89a238387a614b14f702095655d3e0c74
Daniel Wesser <wesser.daniel@gmail.com>
2018-01-11 04:27:26 +0000

    Working on syncing test

--- is-dropbox-awake.sh
+++ is-dropbox-awake.sh
@@ -7,9 +7,10 @@ while [[ $(dropbox status) == "Starting..." ]] ; do
     elif [[ $(dropbox status | grep Indexing) == "Indexing..." ]] ; then
         echo -ne "Dropbox is making a todo list.\r"
         sleep 5
-    elif [[ $(dropbox status | grep Syncing) ]] ; then
-        echo -ne "Dropbox is actually syncing (finally).\r"
-        sleep 5
+    elif [[ $(dropbox status | grep Syncing | cut -d' ' -f 1) == "Syncing" ]]
+        then
+            echo -ne "Dropbox is actually syncing (finally).\r"
+            sleep 5
     else
         echo "IT'S DONE!!!!"
     fi
