295644349c27ba5bd54410f2103b9b7d9a00b875
Daniel Wesser <daniel@elli.local>
2018-02-19 19:48:30 +0000

    Prints table of public IP from multiple sources

---
+++ wanip.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+# This script print the computers WAN ip
+
+# Print to file version
+echo "service   | ip v4 address"
+echo "--------- | -------------"
+echo "icanhazip | "`curl -s https://icanhazip.com/`
+echo "OpenDNS   | "`dig +short myip.opendns.com @resolver1.opendns.com`
+echo "telnet    | "`telnet 4.ifcfg.me 2>&1 | grep IPv4 | cut -d' ' -f4`
+
+# Print to terminal version
+#echo "service   -> ip v4 address"
+#echo "---------    -------------"
+#echo "icanhazip ->" `curl -s https://icanhazip.com/`
+#echo "OpenDNS  -->" `dig +short myip.opendns.com @resolver1.opendns.com`
+#echo "telnet  --->" `telnet 4.ifcfg.me 2>&1 | grep IPv4 | cut -d' ' -f4`
