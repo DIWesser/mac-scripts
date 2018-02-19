939bed91d22778638739ddf36a97d71fa8660dfb
Daniel Wesser <daniel@elli.local>
2018-02-19 19:51:33 +0000

    Only test if command present

--- wanip.sh
+++ wanip.sh
@@ -4,9 +4,15 @@
 # Print to file version
 echo "service   | ip v4 address"
 echo "--------- | -------------"
-echo "icanhazip | "`curl -s https://icanhazip.com/`
-echo "OpenDNS   | "`dig +short myip.opendns.com @resolver1.opendns.com`
-echo "telnet    | "`telnet 4.ifcfg.me 2>&1 | grep IPv4 | cut -d' ' -f4`
+if [[ $(command -v curl) ]] ; then
+    echo "icanhazip | "`curl -s https://icanhazip.com/`
+fi
+if [[ $(command -v dig) ]] ; then
+    echo "OpenDNS   | "`dig +short myip.opendns.com @resolver1.opendns.com`
+fi
+if [[ $(command -v telnet) ]] ; then
+    echo "telnet    | "`telnet 4.ifcfg.me 2>&1 | grep IPv4 | cut -d' ' -f4`
+fi
 
 # Print to terminal version
 #echo "service   -> ip v4 address"
