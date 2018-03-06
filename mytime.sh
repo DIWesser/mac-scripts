2302823a20f42579516b81c0579f4e8bbd65ac03
Daniel Wesser <daniel@elli.local>
2018-03-06 18:24:49 +0000

    Add idea for VPN manager

--- mytime.sh
+++ mytime.sh
@@ -1,9 +1,21 @@
+#!/bin/bash
+#
+# This script prints out the time in the user's preffered non-system timezone.
+# If no preferred timezone is set, it prints the system time.
+# I'm using this because I keep my computer on UTC for the sake of metadata, 
+# but that isn't the timezone where I live.
+
+# If settings file exists
 if [ -f $HOME/.config/my-scripts/device-id ] ; then
+    # Load it
     settings="$HOME/.config/my-scripts/device-id"
+    # Read the timezone from it
     myTimezone=$(grep -iw '^\s*timezone:' $settings | cut -d: -f2 | xargs)
+    # And print the time in the timezone
     env TZ=$myTimezone date +"%H:%M %Z"
+
+# If settings file does not exist
 else
+    # Just print the system time
     date +"%H:%M %Z"
 fi
-
-
--- notes/future projects.md
+++ notes/future projects.md
@@ -1,9 +1,21 @@
-2018-01-07  
+---
+created: 2018-01-07
+title: Future Script Notes
+author: Daniel Wesser
+---
 
 This document is a dumping ground for things I might make at some point.  
 
 ## Mac and Linux Backup Script
 
+Added: 2018-01-07  
+
 - [Counterpart](https://github.com/jedda/Counterpart)
   - This is a rsync 3 wrapper for OSX
   - There do seem to be some interesting tweaks in the [forks](https://github.com/jedda/Counterpart/network).
+
+## VPN Manager
+
+Added: 2018-03-06  
+
+Make a simple script to allow consitant commands for different VPNs. This would make it possible to use all VPNs with `vpn [name] [action]`. For example `vpn dal connect`.  
