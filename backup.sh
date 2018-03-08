1b3ca2dafc32f1f09d0840606e9df1b9b24b1a2d
Daniel Wesser <daniel@elli.local>
2018-03-08 08:54:34 +0000

    Bootable backups should work now

--- backup.sh
+++ backup.sh
@@ -6,26 +6,33 @@
 #sourceDir="$HOME/Desktop/Source/"
 #destDir="$HOME/Desktop/Destination/"
 sourceDir="/"
-destDir="/Volumes/Boot 1"
-archiveRoot="_CCC"
+destDir="/Volumes/DIW Boot 1"
+archiveRoot="_CCC SafetyNet"
 archiveDir="$archiveRoot/$(date +%Y-%m-%d\ \(%B\ %d\)\ %H-%M-%S)"
+
+if [ ! -r "$sourceDir" ]; then
+    echo "Source $sourceDir not readable - Cannot start the sync process"
+    exit;
+fi
 
 #################################################################################
 # OS Variables
 #################################################################################
 if [[ $(uname -s) == Darwin ]] ; then
     os="macOS"
-    exclude=".config/diwesser/backup-exclude-mac"
+    exclude="$HOME/.config/diwesser/backup-exclude-mac"
 fi
 
 rsync \
     --archive \
-    --verbose \
     --backup \
     --backup-dir="$archiveDir" \
     --exclude-from="$exclude" \
+    --append-verify \
+    --partial \
+    --verbose \
     --stats \
     --progress \
     "$sourceDir" \
-    "$destDir" \
-    --dry-run
+    "$destDir" 
+    #--dry-run
