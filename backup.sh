ef18201ecd655cd4e1901fa7a71d4070f27f373c
Daniel Wesser <daniel@T8ACC.WPA.Dal.Ca>
2018-03-08 07:57:19 +0000

    Tweaking source and destination, dry run for now

--- backup.sh
+++ backup.sh
@@ -1,16 +1,31 @@
 #!/bin/bash
 
-sourceDir="$HOME/Desktop/Source/"
-destDir="$HOME/Desktop/Destination/"
-archiveFolder="_CCC"
+#################################################################################
+# Global Variables
+#################################################################################
+#sourceDir="$HOME/Desktop/Source/"
+#destDir="$HOME/Desktop/Destination/"
+sourceDir="/"
+destDir="/Volumes/Boot 1"
+archiveRoot="_CCC"
+archiveDir="$archiveRoot/$(date +%Y-%m-%d\ \(%B\ %d\)\ %H-%M-%S)"
+
+#################################################################################
+# OS Variables
+#################################################################################
+if [[ $(uname -s) == Darwin ]] ; then
+    os="macOS"
+    exclude=".config/diwesser/backup-exclude-mac"
+fi
 
 rsync \
     --archive \
     --verbose \
     --backup \
-    --backup-dir="$archiveFolder" \
-    --exclude=/Volumes/ \
+    --backup-dir="$archiveDir" \
+    --exclude-from="$exclude" \
     --stats \
     --progress \
     "$sourceDir" \
-    "$destDir"
+    "$destDir" \
+    --dry-run
