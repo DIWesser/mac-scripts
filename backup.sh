601e2b2afe7303d5fa7d94101f1cb7116f0af402
Daniel Wesser <daniel@elli.local>
2018-03-08 20:21:34 +0000

    Warn if low space in archive. Reorganize sections. Add comments

--- backup.sh
+++ backup.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+# Know Problems
+#   - Archive size is ound using df -H. This may cause problems if the free space
+#     in the archive is over 1TB
 
 #################################################################################
 # Global Variables
@@ -9,16 +12,7 @@ sourceDir="/"
 destDir="/Volumes/DIW Boot 1/"
 archiveRoot="_CCC SafetyNet"
 archiveDir="$archiveRoot/$(date +%Y-%m-%d\ \(%B\ %d\)\ %H-%M-%S)"
-
-if [ ! -r "$sourceDir" ]; then
-    echo "Source $sourceDir not readable - Cannot start the sync process"
-    exit;
-fi
-
-if [ ! -w "$destDir" ]; then
-    echo "Destination $destDir not writeable - Cannot start the sync process"
-    exit;
-fi
+pruneValue=150  # Minimum archive size in GB
 
 #################################################################################
 # OS Variables
@@ -28,6 +22,29 @@ if [[ $(uname -s) == Darwin ]] ; then
     exclude="$HOME/.config/diwesser/backup-exclude-mac"
 fi
 
+#################################################################################
+# Pre-Run Operations
+#################################################################################
+# Check for source directory
+if [ ! -r "$sourceDir" ]; then
+    echo "Source $sourceDir not readable - Cannot start the sync process"
+    exit;
+fi
+
+# Check for backup directory
+if [ ! -w "$destDir" ]; then
+    echo "Destination $destDir not writeable - Cannot start the sync process"
+    exit;
+fi
+
+# Check archive size
+if [[ $(df -PH . | tail -1 | awk '{print $4}' | cut -d'G' -f1) -lt $pruneValue ]] ; then
+    echo "Warning! Archive is under $pruneValue GB"
+fi
+
+#################################################################################
+# Run
+#################################################################################
 rsync \
     --archive \
     --backup \
