0433e92c68cd77cb0a80df0622c89b0e8bc5b4bf
Daniel Wesser <wesser.daniel@gmail.com>
2018-03-21 13:47:11 +0000

    Added timeout for 'Starting..', added confirmation before celebrating, added exit if message not recognized.

--- is-dropbox-awake.sh
+++ is-dropbox-awake.sh
@@ -1,23 +1,37 @@
 #!/bin/bash
 #
-# This is a stort scrip to monitor whether or not Dropbox has finished syncing.
+# This script monitors for when Dropbox has finished syncing.
 
-# Loop every 5 second until Dropbox says it's done
+startTime=$(date +%s)
+maxWaitTime=300       # Limit before erroring out in seconds
+
+# Loop every 5 seconds until Dropbox says it's done
 while [[ $(dropbox status) != "Up to date" ]] ; do
     # If Dropbox is just starting up
     if [[ $(dropbox status) == "Starting..." ]] ; then
         echo -ne "\rDropbox hasn't woken up yet."
-        sleep 5
+        # Timeout if Dropbox is frozen
+        if [[ $(date +%s) -ge $startTime+$maxWaitTime ]] ; then
+            echo -ne "\rDropbox has been starting for five minutes."
+            echo -ne " You may need to restart it."
+            exit 1
+        fi
     # If Dropbox is indexing
     elif [[ $(dropbox status | grep Indexing) == "Indexing..." ]] ; then
         echo -ne "\rDropbox is making a todo list."
-        sleep 5
     # If Dropbox is syncing
     elif [[ $(dropbox status | grep Syncing | cut -d' ' -f 1) == "Syncing" ]]
         then
             echo -ne "\rDropbox is actually syncing (finally)."
+    elif
+        echo -ne "\rDropbox output an unrecognize message. Exiting script."
+        echo -ne "\nThe output of \`dropbox status\` is:"
+        dropbox status
     fi
     sleep 5
 done
 
-echo "IT'S DONE!!!!"
+# Celebrate if it's finally done
+if [[ $(dropbox status) == "Up to date" ]]
+    echo "IT'S DONE!!!!"
+fi
