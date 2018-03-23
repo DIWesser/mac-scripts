0e3f7915ac45fea496d9f0e4b01c6431551705d7
Daniel Wesser <wesser.daniel@gmail.com>
2018-03-23 06:15:05 +0000

    Added todo to comments

--- select2speech.sh
+++ select2speech.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
+#
+# Takes selected text and reads it.
+# Todo:
+# - Have it find running versions of itself and kill them before it starts. I'm
+#   not a big fan of having multiple robots yelling at me.
+
 xsel | festival --tts
