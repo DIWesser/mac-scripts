a6ff3079b75fd9c8f9d8e32f7099bc1681931e32
Daniel Wesser <wesser.daniel@gmail.com>
2018-04-18 04:14:20 +0000

    Refactored with settings location defined once and moved settings to .config/diwesser/

--- mytime.sh
+++ mytime.sh
@@ -5,14 +5,12 @@
 # I'm using this because I keep my computer on UTC for the sake of metadata, 
 # but that isn't the timezone where I live.
 
+settings="$HOME/.config/diwesser/device-id"
 # If settings file exists
-if [ -f $HOME/.config/my-scripts/device-id ] ; then
-    # Load it
-    settings="$HOME/.config/my-scripts/device-id"
-    # Read the timezone from it
-    myTimezone=$(grep -iw '^\s*timezone:' $settings | cut -d: -f2 | xargs)
-    # And print the time in the timezone
-    env TZ=$myTimezone date +"%H:%M %Z"
+if [ -f $settings ] ; then
+    # Read the timezone from settings and print the time in the timezone
+    env TZ=$(grep -iw '^\s*timezone:' $settings | cut -d: -f2 | xargs) \
+        date +"%H:%M %Z"
 
 # If settings file does not exist
 else
