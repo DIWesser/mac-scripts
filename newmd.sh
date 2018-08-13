8b946799d6eecbd2598d43b7e1334a282247b1fd
Daniel Wesser <daniel@elli.local>
2018-08-13 07:43:14 +0000

    Added more header options, played with order of tasks

--- newmd.sh
+++ newmd.sh
@@ -1,29 +1,67 @@
 #!/bin/bash
 
-timeZone="America/Halifax"
-author="Daniel Wesser"
-title=
-newFile=
-
-headerCreationDate=true
-headerAuthor=true
+# Initialize header field toggles
+headerAuthor=false
 headerCourse=false
-headerLecturer=false
-headerTitle=false
+headerCreationDate=false
 headerDue=false
-headerPublisher=false
-headerUrl=false
+headerLecturer=false
 headerMirrorUrl=false
+headerPublisher=false
 headerTags=false
-printHelp=false
+headerTitle=false
+headerUrl=false
 
 
+# getopts goes here
 
-echo "---" >> $newFile
+
+# Metadata variables
+timeZone="America/Halifax"
+nonSystemTimezone=true
+if [[ $nonSystemTimezone == true ]] ; then
+    date="$(env TZ=$timeZone date +'%Y-%m-%d')"
+else
+    date="$(date +'%Y-%m-%d')"
+fi
+author="Daniel Wesser"
+course=""
+lecturer=""
+directory=$(pwd)
+fileName="$date $title.md"
+filePath="$fileName/$directory"
+title=""
+docType=""
+
+
+# Enable header fields based on document type
+if [[ $docType == "courseLectureNote" ]] ; then
+    headerCreationDate=true
+    headerAuthor=true
+    headerCourse=true
+    headerLecturer=true
+elif [[ $docType == "miscNote" ]] ; then
+else
+    headerCreationDate=true
+    headerTitle=true
+    headerAuthor=true
+fi
+
+
+echo "---" >> $filePath
 if [[ $headerCreationDate == true ]] ; then
-    echo "created: $(env TZ=$timeZone date +'%Y-%m-%d')" >> $newFile
+    echo "created: $(env TZ=$timeZone date +'%Y-%m-%d')" >> $filePath
 fi
+
+if [[ $headerCourse == true ]] ; then
+    echo "$course" >> $filePath
+fi
+
+if [[ $headerLecturer == true ]] ; then
+    echo "$lecturer" >> $filePath
+fi
+
 if [[ $headerAuthor == true ]] ; then
-    echo "author: $author" >> $newFile
+    echo "author: $author" >> $filePath
 fi
-echo "---" >> $newFile
+echo "---" >> $filePath
