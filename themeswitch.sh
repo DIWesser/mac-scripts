5a8556b87e1e5d4c37ffcbd9f1265e791077a137
Daniel Wesser <wesser.daniel@gmail.com>
2018-09-08 15:33:19 +0000

    Chooses theme based on time

--- themeswitch.sh
+++ themeswitch.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
 
-debugOutput=false
+# Todo
+# - dayNight is really crude and can't handle time wrapping
 
+debugOutput=false
+timeZone="America/Halifax"          # Enter "system" to use system time
+location="CAXX0183"                 # Location for weather.com
+dayTheme="Minwaita-Vanilla"         # Light theme
+nightTheme="Minwaita-Vanilla-Dark"  # Dark theme
+dayTimeStart="0630"                # Start of day colours
+nightTimeStart="2000"              # Start of night colours
+
+
+# Change GTK theme
+# - Takes theme name as string
 setTheme() {
     local theme="$1"
-    if [[ $debugOutput ]] ; then
+    if [[ $debugOutput == true ]] ; then
         echo "\`setTheme\` function detects theme as $theme."
         echo "Changing theme."
     fi
@@ -12,5 +24,31 @@ setTheme() {
     gsettings set org.gnome.desktop.wm.preferences theme "$theme"
 }
 
-#setTheme "Minwaita-Vanilla-Dark"
-setTheme "Minwaita-Vanilla"
+
+# Find sunrise and sunset time
+# - Takes no input, sets global variables sunsetTime and sunriseTime
+sunriseSunset(){
+    # Obtain sunrise and sunset raw data from weather.com
+    sun_times=$( curl -s  https://weather.com/weather/today/l/$location | sed 's/<span/\n/g' | sed 's/<\/span>/\n/g'  | grep -E "dp0-details-sunrise|dp0-details-sunset" | tr -d '\n' | sed 's/>/ /g' | cut -d " " -f 4,8 )
+    echo "Page downloaded"
+
+sunriseSunset
+    # Extract sunrise and sunset times and convert to 24 hour format
+    sunriseTime=$(date --date="`echo $sun_times | awk '{ print $1}'` AM" +%R)
+    sunsetTime=$(date --date="`echo $sun_times | awk '{ print $2}'` PM" +%R)
+}
+
+
+# Finds if current time is in defined day period or night period.
+# Takes no input, returns "day" or "night"
+dayNight(){
+    local currentTime=$(env TZ="$timeZone" date +"%H%M")
+    (( "$dayTimeStart" < "$currentTime" && "$currentTime" < "$nightTimeStart" ))\
+        && echo day || echo night
+}
+
+if [[ $(dayNight) == "day" ]] ; then
+    setTheme "Minwaita-Vanilla"
+else
+    setTheme "Minwaita-Vanilla-Dark"
+fi
