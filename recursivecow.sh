d9bfea6973c2bff8dd07a03ccc3c7865e6bb16f9
Daniel Wesser <daniel@elli.local>
2018-12-23 21:48:07 +0000

    Demo of cowsay udes recursively.

---
+++ recursivecow.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+clear
+cowsay So, I've probably mentioned that there's this CLI program called "cowsay".
+sleep 3 && clear
+cowsay "It does pretty much what it says on the tin."
+sleep 3 && clear
+cowsay Well, the other day someone on reddit mentioned that it can be run recursively...
+sleep 3 && clear
+cowsay Well, the other day someone on reddit mentioned that it can be run recursively... | cowsay -n
+sleep 3 && clear
+cowsay Well, the other day someone on reddit mentioned that it can be run recursively... | cowsay -n | cowsay -n
+sleep 1 && clear
+cowsay Well, the other day someone on reddit mentioned that it can be run recursively... | cowsay -n | cowsay -n | cowsay -n
+sleep 1 && clear
+cowsay Well, the other day someone on reddit mentioned that it can be run recursively... | cowsay -n | cowsay -n | cowsay -n | cowsay -n
+sleep 1 && clear
+cowsay Well, the other day someone on reddit mentioned that it can be run recursively... | cowsay -n | cowsay -n | cowsay -n | cowsay -n | cowsay -n
+sleep 1 && clear
+cowsay Well, the other day someone on reddit mentioned that it can be run recursively... | cowsay -n | cowsay -n | cowsay -n | cowsay -n | cowsay -n | cowsay -n
+sleep 1 && clear && sleep 1
+cowsay Nerds are weird | lolcat
