ead24916c01c823e4e34e696ec991124995889d5
Daniel Wesser <wesser.daniel@gmail.com>
2019-04-29 23:06:16 -0300

    Add explain function. Not yey used.

--- when_will_i_graduate.sh
+++ when_will_i_graduate.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+#
+# Todo:
+# - Integrate explain function.
 
 # Save current year to variable
 current_year="$(echo $(date +'%Y'))"
@@ -16,6 +19,12 @@ grad_year () {
 # Formats grad year as answer and prints
 give_answer () {
     echo "You will graduate by $(grad_year)."
+}
+
+# Describe how program works
+explain () {
+    echo "This program takes the current year and adds three years."
+    echo "Using this formula, it has found that you will graduate in $(grad_year)"
 }
 
 main () {
