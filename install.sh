252c6ff2b0591c276962ba540f66e2e31ffec563
Daniel Wesser <wesser.daniel@gmail.com>
2019-07-02 07:26:46 -0300

    added several scripts to default install

--- install.sh
+++ install.sh
@@ -17,23 +17,31 @@ iconDest="$HOME/.local/share/icons"
 mkdir -p ~/bin
 
 # Link scripts
-ln -s $sourceFolder/upgrade-all.sh    $linkFolder/upgrade-all
-ln -s $sourceFolder/pomodoro.sh       $linkFolder/pomodoro
-ln -s $sourceFolder/journal.sh        $linkFolder/journal
-ln -s $sourceFolder/mytime.sh         $linkFolder/mytime
-ln -s $sourceFolder/otfplay.sh        $linkFolder/otfplay
-ln -s $sourceFolder/pomodoro.sh       $linkFolder/pomodoro
-ln -s $sourceFolder/themeswitch.sh    $linkFolder/themeswitch
-ln -s $sourceFolder/timer.sh          $linkFolder/timer
-ln -s $sourceFolder/wanip.sh          $linkFolder/wanip
-ln -s $sourceFolder/calc.sh           $linkFolder/calc
+ln -s $sourceFolder/calc.sh             $linkFolder/calc
+ln -s $sourceFolder/import-notes.sh     $linkFolder/import-notes
+ln -s $sourceFolder/is-dropbox-awake.sh $linkFolder/is-dropbox-awake
+ln -s $sourceFolder/journal.sh          $linkFolder/journal
+ln -s $sourceFolder/linuxcontrol.sh     $linuxcontrol/linuxcontrol
+ln -s $sourceFolder/mytime.sh           $linkFolder/mytime
+ln -s $sourceFolder/newclassnote.sh     $linkFolder/newclassnote
+ln -s $sourceFolder/newmd.sh            $linkFolder/newmd
+ln -s $sourceFolder/newvoid.sh          $linkFolder/newvoid
+ln -s $sourceFolder/otfplay.sh          $linkFolder/otfplay
+ln -s $sourceFolder/pomodoro.sh         $linkFolder/pomodoro
+ln -s $sourceFolder/say.sh              $linkFolder/say
+ln -s $sourceFolder/straywords.sh       $linkFolder/straywords
+ln -s $sourceFolder/themeswitch.sh      $linkFolder/themeswitch
+ln -s $sourceFolder/timer.sh            $linkFolder/timer
+ln -s $sourceFolder/toggledarktheme     $linkFolder/toggledarktheme
+ln -s $sourceFolder/upgrade-all.sh      $linkFolder/upgrade-all
+ln -s $sourceFolder/wanip.sh            $linkFolder/wanip
 
 # Link .desktop files
-ln -s $desktopSource/daynight.desktop $desktopDest/daynight.desktop
-ln -s $desktopSource/pocket.desktop   $desktopDest/pocket.desktop
-ln -s $desktopSource/sep.desktop      $desktopDest/sep.desktop
+ln -s $desktopSource/daynight.desktop   $desktopDest/daynight.desktop
+ln -s $desktopSource/pocket.desktop     $desktopDest/pocket.desktop
+ln -s $desktopSource/sep.desktop        $desktopDest/sep.desktop
 
 # Link icons
-ln -s $iconSource/daynight.png        $iconDest/daynight.png
-ln -s $iconSource/pocket.png          $iconDest/pocket.png
-ln -s $iconSource/sep.png             $iconDest/sep.png
+ln -s $iconSource/daynight.png          $iconDest/daynight.png
+ln -s $iconSource/pocket.png            $iconDest/pocket.png
+ln -s $iconSource/sep.png               $iconDest/sep.png
