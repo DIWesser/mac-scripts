ff025ccf7d58a469629cb86d167178f7bf694de1
Daniel Wesser <wesser.daniel@gmail.com>
2019-11-19 18:05:45 -0400

    Test for dependencies and change colours a couple times

--- recursivecow.sh
+++ recursivecow.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
+if ! [[ -x "$(command -v cowsay)" ]]; then
+    echo "Exiting. cowsay is not installed"
+    exit 0
+fi
+
+if ! [[ -x "$(command -v lolcat)" ]]; then
+    echo "Exiting. lolcat is not installed"
+    exit 0
+fi
+
 clear
-cowsay So, I've probably mentioned that there's this CLI program called "cowsay".
+cowsay "So, I've probably mentioned that there's this CLI program called \"cowsay\"".
 sleep 3 && clear
 cowsay "It does pretty much what it says on the tin."
 sleep 3 && clear
@@ -19,4 +29,8 @@ cowsay Well, the other day someone on reddit mentioned that it can be run recurs
 sleep 1 && clear
 cowsay Well, the other day someone on reddit mentioned that it can be run recursively... | cowsay -n | cowsay -n | cowsay -n | cowsay -n | cowsay -n | cowsay -n
 sleep 1 && clear && sleep 1
+cowsay Nerds are weird | lolcat
+sleep 1 && clear
+cowsay Nerds are weird | lolcat
+sleep 1 && clear
 cowsay Nerds are weird | lolcat
