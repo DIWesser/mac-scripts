0ffa1beeaafff6fe819690e1bae2de6a1b292019
Daniel Wesser <wesser.daniel@gmail.com>
2020-02-07 04:59:53 -0400

    Complete rewrite. Now loops through scripts and .desktop files asking for confirmation.

--- install.sh
+++ install.sh
@@ -1,48 +1,66 @@
 #!/bin/bash
 
-# This script should symlink the everything that works
-
 # Scripts are stored in ~/bin
 # To access add the following line to .bashrc/bash_profile
 # export PATH=~/bin:$PATH
 
-sourceFolder="$HOME/git/scripts"
+
+sourceFolder="$(pwd)"
 linkFolder="$HOME/bin"
 desktopSource="$sourceFolder/desktop_files"
 desktopDest="$HOME/.local/share/applications"
 iconSource="$sourceFolder/icons"
 iconDest="$HOME/.local/share/icons"
 
-# Create directory
-mkdir -p "$HOME/bin"
-mkdir -p "$HOME/.local/share/icons"
 
-# Link scripts
-ln -s $sourceFolder/calc.sh             $linkFolder/calc
-ln -s $sourceFolder/import-notes.sh     $linkFolder/import-notes
-ln -s $sourceFolder/is-dropbox-awake.sh $linkFolder/is-dropbox-awake
-ln -s $sourceFolder/journal.sh          $linkFolder/journal
-ln -s $sourceFolder/linuxcontrol.sh     $linuxcontrol/linuxcontrol
-ln -s $sourceFolder/mytime.sh           $linkFolder/mytime
-ln -s $sourceFolder/newclassnote.sh     $linkFolder/newclassnote
-ln -s $sourceFolder/newmd.sh            $linkFolder/newmd
-ln -s $sourceFolder/newvoid.sh          $linkFolder/newvoid
-ln -s $sourceFolder/otfplay.sh          $linkFolder/otfplay
-ln -s $sourceFolder/pomodoro.sh         $linkFolder/pomodoro
-ln -s $sourceFolder/say.sh              $linkFolder/say
-ln -s $sourceFolder/straywords.sh       $linkFolder/straywords
-ln -s $sourceFolder/themeswitch.sh      $linkFolder/themeswitch
-ln -s $sourceFolder/timer.sh            $linkFolder/timer
-ln -s $sourceFolder/toggledarktheme     $linkFolder/toggledarktheme
-ln -s $sourceFolder/upgrade-all.sh      $linkFolder/upgrade-all
-ln -s $sourceFolder/wanip.sh            $linkFolder/wanip
+# Takes full path then root path
+between_root_and_ext () {
+    fullPath="$1"
+    rootPath="$2"
+    noRootWithExt="${fullPath#$rootPath}"
+    noRootOrExt="${noRootWithExt%.*}"
+    echo "$noRootOrExt"
+}
 
-# Link .desktop files
-ln -s $desktopSource/daynight.desktop   $desktopDest/daynight.desktop
-ln -s $desktopSource/pocket.desktop     $desktopDest/pocket.desktop
-ln -s $desktopSource/sep.desktop        $desktopDest/sep.desktop
 
-# Link icons
-ln -s $iconSource/daynight.png          $iconDest/daynight.png
-ln -s $iconSource/pocket.png            $iconDest/pocket.png
-ln -s $iconSource/sep.png               $iconDest/sep.png
+install_scripts () {
+    mkdir -p "$linkFolder"
+
+    for script in ./*.sh
+    do
+        script=${script:2}
+        scriptTitle="$(between_root_and_ext '$script' '$sourceFolder')"
+        if [ "$script" != *install.sh ] ; then
+            echo -n "Do you want to link $script? [y/N]: "
+            read install
+            if [[ "$install" = Y ]] || [[ "$install" == y ]] ; then
+                ln -s  "$sourceFolder/$script" "$linkFolder/$scriptTitle"
+            fi
+        fi
+    done
+}
+
+
+install_dot_desktop () {
+    mkdir -p "$desktopDest"
+    mkdir -p "$iconDest"
+
+    for desktop in "$desktopSource/"*.desktop
+    do
+        desktopTitle="$(between_root_and_ext '$desktop' '$desktopSource')"
+        desktopTitle="${desktopTitle:1}"
+        echo -n "Would you like to link $desktopTitle.desktop? [y/N]: "
+        read install
+        if [[ "$install" == Y ]] || [[ "$install" == y ]] ; then
+            ln -s "$desktopSource/$desktopTitle.desktop" \
+                "$desktopDest/$desktopTitle.desktop"
+            ln -s "$iconSource/$desktopTitle.png" "$iconDest/$desktopTitle.png"
+        fi
+    done
+}
+
+
+# main
+install_scripts
+install_dot_desktop
+echo "Install complete. Note that \$PATH must include \$HOME/bin for scripts to be accessed."
