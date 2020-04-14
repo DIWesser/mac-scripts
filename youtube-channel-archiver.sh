29018b1c164729db9f0590757a5adaea41d66782
Daniel Wesser <wesser.daniel@gmail.com>
2020-04-14 20:55:19 -0300

    Tweaked youtube-dl options, actually ready now

--- youtube-channel-archiver.sh
+++ youtube-channel-archiver.sh
@@ -1,27 +1,36 @@
 #!/bin/bash
 # This script is used to create and update local archives of Youtube channels
 
-youtube_archive_dir="/run/media/daniel/DIW Storage 0/video/youtube channels"
+# Todo:
+# - Option flag for archive_dir.
+# - Transcode to HEVC (hint: `--exec`).
+# - Option to only update a specific channel.
+# - Option to add a channel with this script.
+# - Allow for per channel config file (e.g. parsing consistantly formatted
+#   channel titles to extract actual video title).
+# - Use the source name from the download archive file instead of the hardcoded
+#   'Youtube ID:' in the [Youtube ID: 123xyz] tag.
 
-#add_channel () {}
-#update_single_channel () {}
+archive_dir="/run/media/daniel/DIW Storage 0/video/youtube channels"
 
 download () {
     youtube-dl \
-        --format best \
         --download-archive downloaded \
         --continue \
         --ignore-errors \
         --no-overwrites \
+        --add-metadata \
+        --write-thumbnail \
         --embed-thumbnail \
-        --write-description \
+        --all-subs \
+        --embed-subs \
         --write-info-json \
         --output "%(upload_date)s %(title)s [%(resolution)s] [Youtube ID: %(id)s].%(ext)s" \
         "$1"
 }
 
 update_all_channels () {
-    for i in "$youtube_archive_dir"/*; do
+    for i in "$archive_dir"/*; do
         cd "$i"
         download "$(cat channel_url)"
     done
