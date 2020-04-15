17ffd751a14b14d81d6e455dbff4996b283e6205
Daniel Wesser <wesser.daniel@gmail.com>
2020-04-15 19:41:03 -0300

    Create is_subbed_test and use it in new_sub, add main with 'update' and 'add' arguments

--- youtube-channel-archiver.sh
+++ youtube-channel-archiver.sh
@@ -3,27 +3,46 @@
 
 # Todo:
 # - Option flag for archive_dir.
-# - Transcode to HEVC (hint: `--exec`).
+# - Transcode to HEVC (hint: `youtbe-dl --exec`).
 # - Option to only update a specific channel.
-# - Option to add a channel with this script.
+# - Confirm folder name when adding new channel.
 # - Allow for per channel config file (e.g. parsing consistantly formatted
 #   channel titles to extract actual video title).
-# - Use the source name from the download archive file instead of the hardcoded
-#   'Youtube ID:' in the [Youtube ID: 123xyz] tag.
+# - Confirm dir has a channel_url file.
 
 archive_dir="/run/media/daniel/DIW Storage 0/video/youtube channels"
+alias printf='builtin printf'
 
-new_subscription () {
-    local url=""
-    local channel=""
-    echo -ne "Enter channel URL: "
-    read "url"
-    channel="$(youtube-dl --get-filename --playlist-end 1 --output '%(uploader)s' $url)"
-    mkdir "$archive_dir"/"$channel"
-    echo "$channel_url" > "$archive_dir"/"$channel"/channel_url
+# Takes url, returns channel directory name or empty string.
+# The 'channel_url' file exists test is hack to deal with empty input. It should
+# be replaced with input validation.
+function is_subbed_test () {
+    for i in "$archive_dir"/* ; do
+        if [[ -f "$i"/channel_url && $(cat "$i"/channel_url) == "$1" ]] ; then
+            printf "${i##*/}"
+            exit 1
+        fi
+    done
+    printf ""
 }
 
-download () {
+function new_sub () {
+    local url=""
+    local channel=""
+    printf "Enter channel URL: "
+    read url
+    local existing_dir=$(is_subbed_test "$url")
+    if [[ "$existing_dir" != "" ]] ; then
+        printf "This channel is already being saved to \'$existing_dir\'.\n"
+    else
+        channel="$(youtube-dl --get-filename --playlist-end 1 --output '%(uploader)s' $url)"
+        mkdir "$archive_dir"/"$channel"
+        printf "$url" > "$archive_dir"/"$channel"/channel_url
+        printf "Channel added. Videos will be saved to \'$channel\'.\n"
+    fi
+}
+
+function download () {
     youtube-dl \
         --download-archive downloaded \
         --continue \
@@ -35,15 +54,26 @@ download () {
         --all-subs \
         --embed-subs \
         --write-info-json \
-        --output "%(upload_date)s %(title)s [Youtube ID: %(id)s].%(ext)s" \
+        --output "%(upload_date)s %(title)s [Video ID: %(id)s].%(ext)s" \
         "$1"
 }
 
-update_all_channels () {
+function update_all_channels () {
     for i in "$archive_dir"/*; do
         cd "$i"
         download "$(cat channel_url)"
     done
 }
 
-update_all_channels
+function main () {
+    if [[ "$1" == "update" ]] ; then
+        update_all_channels
+    elif [[ "$1" == "add" ]] ; then
+        new_sub
+    else
+        printf "Command not recognized. Recognized commands are 'update' and "
+        printf "'add'\n"
+    fi
+}
+
+main "$@"
